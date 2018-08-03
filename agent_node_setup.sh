6f7196de26866e77aab21d4f22a089a01860ccb7
Taylor Herson <therson@hortonworks.com>
2018-08-03 12:11:00 -0400

    hello

--- agent_node_setup.sh
+++ agent_node_setup.sh
@@ -11,6 +11,9 @@ echo HOSTNAME=$host >> /etc/sysconfig/network
 
 setenforce 0
 
+wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.0.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+
+
 yum install -y ambari-agent
 sed -i 's/hostname=.*/hostname=master0.hdpcloud.internal/g' /etc/ambari-agent/conf/ambari-agent.ini
 
